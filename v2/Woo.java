45133a471863baac7ecda5b41de47091bdd30b55
phuang30 <phuang30@stuy.edu>
2022-06-02 13:19:48 -0400

    versions

---
+++ v2/Woo.java
@@ -0,0 +1,86 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.image.*;
+import java.io.*;
+import javax.imageio.*;
+import javax.swing.*;
+public class Woo {
+  static String text;
+  static int count = 0;
+  private static void delay( int n )
+  {
+    try {
+      Thread.sleep(n);
+    } catch( InterruptedException e ) {
+      System.exit(0);
+    }
+  }
+  public static void main(String[] args) {
+
+      JFrame f = new JFrame("Load Image Sample");
+
+      f.addWindowListener(new WindowAdapter(){
+              public void windowClosing(WindowEvent e) {
+                  System.exit(0);
+              }
+          });
+
+      LoadImageApp frameImage = new LoadImageApp("US");
+      frameImage.setBounds(200,200,1280,640);
+      f.add(frameImage);
+      frameImage.setBlank();
+          frameImage.reSize(2);
+
+      JLabel title = new JLabel("Welcome to Narnia");
+      title.setBounds(0,0,200,100);
+      f.add(title);
+
+
+
+      JTextField textField = new JTextField(20);
+      textField.addActionListener(new ActionListener() {
+        public void actionPerformed(ActionEvent evt) {
+          String text = textField.getText();
+          if (text.equals(frameImage.getText())) {
+            frameImage.changeFlag();
+          }
+        }
+      });
+      textField.setBounds(200,0,200,200);
+      f.add(textField);
+
+      JButton ton1 = new JButton("Edit");
+      ton1.addActionListener(new ActionListener() {
+        public void actionPerformed(ActionEvent evt) {
+          frameImage.changeFlag();
+        }
+      });
+      ton1.setMnemonic(KeyEvent.VK_E);
+      ton1.setBounds(0,100,200,100);
+      f.add(ton1);
+
+      f.setSize(2000,1000);
+      f.setLayout(null);
+
+      f.setVisible(true);
+
+      while (true) {
+          delay(500);
+          frameImage.addPixelsFuzz(20);
+          System.out.println("painted" + ", " + frameImage.getText());
+          delay(500);
+          frameImage.reSize(3);
+          System.out.println("resized");
+          delay(500);
+          frameImage.repaint();
+          System.out.println("repaint");
+
+
+        }
+
+
+
+
+
+  }
+}
---
+++ v3/LoadImageApp.java
@@ -0,0 +1,239 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.image.*;
+import java.io.*;
+import javax.imageio.*;
+import javax.swing.*;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+
+/**
+ * This class demonstrates how to load an Image from an external file
+ */
+public class LoadImageApp extends Component {
+    String[] countries = {"AD","AE","AF","AG","AI","AL","AM","AO","AQ","AR","AS","AT","AU","AW","AX","AZ","BA","BB","BD","BE","BF","BG","BH","BI","BJ","BL","BM","BN","BO","BQ","BR","BS","BT","BV","BW","BY","BZ","CA","CA","CC","CD","CF","CG","CH","CI","CK","CL","CM","CN","CO","CR","CU","CV","CW","CX","CY","CZ","DE","DJ","DK","DM","DO","DZ","EC","EE","EG","EH","ER","ES","ET","EU","FI","FJ","FK","FM","FO","FR","GA","GB","GD","GE","GF","GG","GH","GI","GL","GM","GN","GP","GQ","GR","GS","GT","GU","GW","GY","HK","HM","HN","HR","HT","HU","ID","IE","IL","IM","IN","IO","IQ","IR","IS","IT","JE","JM","JO","JP","KE","KG","KH","KI","KM","KN","KP","KR","KW","KY","KZ","LA","LB","LC","LI","LK","LR","LS","LT","LU","LV","LY","MA","MC","MD","ME","MF","MG","MH","MK","ML","MM","MN","MO","MP","MQ","MR","MS","MT","MU","MU","MV","MW","MX","MY","MZ","NA","NC","NE","NF","NG","NI","NL","NO","NP","NR","NU","NZ","OM","PA","PE","PF","PG","PH","PK","PL","PM","PN","PR","PS","PT","PW","PY","QA","RE","RO","RS","RU","RW","SA","SB","SC","SD","SE","SG","SH","SI","SJ","SK","SL","SM","SN","SO","SR","SS","ST","SV","SX","SY","SZ","TC","TD","TF","TG","TH","TJ","TK","TL","TM","TN","TO","TR","TT","TV","TW","TZ","UA","UG","UM","US","UY","UZ","VA","VC","VE","VG","VI","VN","VU","WF","WS","YE","YT","ZA","ZM","ZW"};
+
+    BufferedImage img;
+    BufferedImage img2;
+    BufferedImage resize;
+    int _size;
+    String image;
+    int eRows;
+    ArrayDeque<Integer>[] user;
+    String text;
+    public void paint(Graphics g) {
+        g.drawImage(resize, 0, 0, null);
+    }
+
+    public LoadImageApp() {
+      image = "PNG-128/WF-128.png";
+      text = "WF";
+       try {
+           img = ImageIO.read(new File(image));
+       } catch (IOException e) {
+       }
+       try {
+           img2 = ImageIO.read(new File(image));
+       } catch (IOException e) {
+       }
+       try {
+           resize = ImageIO.read(new File(image));
+       } catch (IOException e) {
+       }
+       _size = 1;
+       eRows = 0;
+       user = dubq(img.getWidth(), img.getHeight());
+    }
+    public LoadImageApp(String n) {
+      image = "PNG-128/" + n + "-128.png";
+      text = n;
+       try {
+           img = ImageIO.read(new File(image));
+       } catch (IOException e) {
+       }
+       try {
+           img2 = ImageIO.read(new File(image));
+       } catch (IOException e) {
+       }
+       try {
+           resize = ImageIO.read(new File(image));
+       } catch (IOException e) {
+       }
+       _size = 1;
+       eRows = 0;
+       user = dubq(img.getWidth(),img.getHeight());
+    }
+
+
+    public String getText() {
+      return text;
+    }
+
+
+    public Dimension getPreferredSize() {
+        if (img == null) {
+             return new Dimension(600,600);
+        } else {
+           return new Dimension(img.getWidth(null) * _size, img.getHeight(null) * _size);
+       }
+    }
+
+    public void reSize (int i) {
+      int width = img.getWidth();
+      int height = img.getHeight();
+      _size = i;
+
+      resize = new BufferedImage(width * i , height * i , 1);
+      int Bwidth = resize.getWidth();
+      int Bheight = resize.getHeight();
+      for (int xcord = 0; xcord < (width * i); xcord += i) {
+        for (int ycord = 0; ycord < (height * i); ycord += i) {
+
+          for (int count = 0; count < i; count ++) {
+            for (int count2 = 0; count2 < i; count2 ++) {
+
+                resize.setRGB(xcord + count, ycord + count2, img.getRGB((xcord / i), (ycord / i)));
+            }
+
+          }
+        }
+
+      }
+    }
+
+    public void transfer () {
+      int width = img.getWidth();
+      int height = img.getHeight();
+      int Bwidth = resize.getWidth();
+      int Bheight = resize.getHeight();
+
+      for (int xcord = 0; xcord < width ; xcord ++) {
+        for (int ycord = 0; ycord < height  ; ycord ++) {
+
+          for (int count = 0; count < _size; count +=_size) {
+            for (int count2 = 0; count2 < _size; count2 +=_size) {
+                if ( (xcord / _size) > width || (ycord / _size) > height || (xcord + count ) > Bwidth || (ycord + count2) > Bheight || (xcord / _size) < 0 || (ycord / _size) < 0 || (xcord + count ) < 0 || (ycord + count2) < 0) {
+                  System.out.println("bad");
+                }
+                resize.setRGB(xcord + count, ycord + count2, img.getRGB((xcord / _size), (ycord / _size)));
+            }
+            }
+
+          }
+        }
+
+
+    }
+
+    public void setBlank() {
+      int width = img.getWidth();
+      int height = img.getHeight();
+      for (int xcord = 0; xcord < width ; xcord ++) {
+        for (int ycord = 0; ycord < height; ycord ++) {
+          img.setRGB(xcord,ycord,-1);
+        }
+
+      }
+
+      user = dubq(img.getWidth(),img.getHeight());
+      eRows = 0;
+      transfer();
+
+    }
+
+    public void addPixelsFuzz(int repeat) {
+      int width = img.getWidth();
+      int height = img.getHeight();
+      int i = 0;
+      while (eRows < img.getWidth() && i < repeat) {
+        int randRow = (int)(Math.random() * width);
+        int x = randRow;
+        int y = 0;
+        if (user[randRow] == null){
+
+        }
+        else if (user[randRow].isEmpty()) {
+          eRows++;
+          user[randRow] = null;
+        }
+        else {
+          y=user[randRow].remove();
+          img.setRGB(x,y, img2.getRGB(x, y));
+          i++;
+        }
+      }
+      transfer();
+    }
+
+    // public void addPixelsSeg(int repeat, int seg) {
+    //
+    //
+    //   int width = img.getWidth() ;
+    //   int height = img.getHeight();
+    //
+    //
+    //   for (int reps = 0; reps < repeat; reps++) {
+    //     int w = (int)(Math.random() * (width - seg) );
+    //     int h = (int)(Math.random() * (height - seg));
+    //     for (int ww = 0; ww < seg; ww ++) {
+    //       for (int hh = 0; hh < seg; hh ++) {
+    //         img.setRGB(w+ww,h+hh, img2.getRGB(w+ww, h+hh));
+    //     }
+    //     }
+    //
+    //   }
+    //   reSize(_size);
+    //
+    // }
+
+    public ArrayDeque<Integer>[] dubq(int x, int y) {
+      ArrayList<Integer> xx = new ArrayList<Integer>();
+      ArrayList<Integer> yy = new ArrayList<Integer>();
+      ArrayDeque<Integer>[] retVal = new ArrayDeque[x];
+      for(int Y = 0; Y < y; Y++) {
+        yy.add(Y);
+      }
+      for(int X = 0; X < x; X++) {
+        xx.add(X);
+      }
+
+      for (int w = 0; w < x ; w ++) {
+        ArrayList<Integer> temp = new ArrayList<Integer>();
+        for(int Y = 0; Y < y; Y++) {
+          temp.add(Y);
+        }
+
+        for (int ww = 0; (!(temp.isEmpty())); ww++) {
+          int randy = (int)(Math.random() * temp.size());
+          int val = temp.remove(randy);
+          if (retVal[w] == null) {
+            retVal[w]=new ArrayDeque<Integer>();
+          }
+
+          retVal[w].add(val);
+        }
+
+      }
+      return retVal;
+    }
+
+    public void changeFlag() {
+      int rando = (int)(Math.random() * countries.length);
+      String newC = countries[rando];
+      text = newC;
+      image = "PNG-128/" + newC + "-128.png";
+       try {
+           img = ImageIO.read(new File(image));
+       } catch (IOException e) {
+       }
+       try {
+           img2 = ImageIO.read(new File(image));
+       } catch (IOException e) {
+       }
+       try {
+           resize = ImageIO.read(new File(image));
+       } catch (IOException e) {
+       }
+       setBlank();
+    }
+}
